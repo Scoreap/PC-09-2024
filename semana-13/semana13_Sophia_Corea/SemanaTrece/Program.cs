24578f8acb7b992096baf5451f5619f60533fa88
Scoreap <157499534+Scoreap@users.noreply.github.com>
2024-04-30 07:26:03 -0600

    Add files via upload

---
+++ semana-13/semana13_Sophia_Corea/SemanaTrece/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace SemanaTrece
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo1 = new int[8];
+            int num = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Ingrese 8 dígitos enteros");
+                num = Int32.Parse(Console.ReadLine());
+                
+                arreglo1[i] = num;
+    
+            }
+            Console.WriteLine("Los dígitos ingresados son: ");
+            foreach(int d in arreglo1)
+            {
+                Console.WriteLine(d);
+            
+            }
+            int suma = 0;
+            for(int i = 0; i < 8; i++ )
+            {
+            
+                suma = arreglo1[i] + suma;
+
+            }
+            Console.WriteLine("La suma de los dígitos es: " + suma);
+
+            int promedio = suma / arreglo1.Length;
+            Console.WriteLine("El promedio es: " + promedio);
+            
+    }
+}
+}
---
+++ semana-13/semana13_Sophia_Corea/SemanaTrece2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SemanaTrece
+{
+    class Program
+    {
+        // No funcionó
+        static void Main(string[] args)
+        {
+            string[,] arreglo2 = new string[4,4];
+            string nombre;
+            string apellido;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("Ingrese hasta 4 nombres: ");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese los respectivos apellidos: ");
+                    apellido = Console.ReadLine();
+                    arreglo2[i,j] = nombre;
+                }
+            Console.WriteLine("Los datos ingresados son: " + arreglo2);
+            }
+               
+            
+    }
+}
+    }
